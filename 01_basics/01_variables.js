a1cfb46d94c1d1ab09b3e450670c046a0b3ccf06
Aatifequbal <atifequbal1916@gmail.com>
2025-12-22 22:23:36 +0000

    add variable declarations and initial values

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345
+let accountEmail = "aatif12@gmail.com"
+var accountPassword = "212121456"
+accountCity = "jaipur"
+let accountState;
+
+//  accountId =2 // not allowed
+ console.log(accountId);
+ 
+
+accountEmail ="hc@gmail.com"
+accountPassword = "2121121"
+accountCity = "bangaluru"
+
+/*
+prefer not use var
+because of issue in block scope & functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
