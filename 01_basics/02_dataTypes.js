f8844322c8105e4e7d267b19a3774c57502d9965
Aatifequbal <atifequbal1916@gmail.com>
2025-12-23 21:31:03 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as never version
+
+// alert ( 3+3) // we are using node js not browser
+
+console.log(3 + 3)
+// code redability should be high
+console.log("aatif")
+
+
+let name = "Aatif"        // String
+let age = 22              // int or number
+let isLoggiedIn = false   // boolean
+let state;
+let date = undefined
+
+
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true / false
+// null => standlone value
+// undefined
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof null)     // null-type-->object
+console.log(typeof undefined) // undefined-type -->undefined 
+ 
+
