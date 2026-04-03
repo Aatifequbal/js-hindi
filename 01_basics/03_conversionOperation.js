0c86dca963a29ed3898b0578cc89ce2f034fe8e8
Aatifequbal <atifequbal1916@gmail.com>
2026-04-03 11:01:35 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score =  "33asd"
+//let score =  33
+// console.log(typeof score);  //  output number -  lowerCase
+// console.log(typeof (score)); //  output number  -lowerCase
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); 
+// console.log( valueInNumber); 
+
+
+
+// "33"  => 33
+// "33qwe"  => NaN
+// true => 1
+// false => 0
+
+let isLogedIn = 1;
+
+let booleanIsLogedIn = Boolean(isLogedIn);
+console.log(booleanIsLogedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "aatif" => true
